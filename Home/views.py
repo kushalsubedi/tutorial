b075f17d3abaafa9c3bd0e00c0be8bb9770dd966
kushal <kushal@Debian.ip6.wlink.com.np>
2024-03-29 16:11:03 +0545

    Day-2 Django Tutorial

--- Home/views.py
+++ Home/views.py
@@ -1,26 +1,67 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# Create your views here.
+
 
 def index (request):
-    return  HttpResponse("Hello World")
+    return  render(request,'Home/index.html')
+
 
 def about (request):
-    return  HttpResponse("""
-                         
-                         <style>
-                         span {
-                         color: red;
-                         }
-                         
-                          </style>
-                         <h1> Task for you </h1> 
+    owner = {
 
-                            <p> 1. Create 2 <span> new app <span> </p>
-                            <p> create urls.py in both app </p>
-                            <p> create some views in both app  and make sure they were access in web</p>   
-                         
-                         
-                         """)
+        "name":"kushal",
+        "age":24,
+        "salary":50_000
+
+    }
+
+    general = {
+        "copyright":"2024",
+        "location":"Pokhara"
+    }
+
+    context = {
+        "owner":owner,
+        "general":general
+    }
+
+    return  render(request,'Home/about.html',context)
+
+
+def Months(request):
+    
+
+    janauary = {
+        "name":"jan",
+        "days":31,
+        "season":"winter"
+    }
+
+    April = {
+        "name":"apr",
+        "days":28,
+        "season":"summer"
+    }
+
+    march = {
+        "name":"mar",
+        "days":31,
+        "season":"spring"
+    }
+
+
+
+
+    context = {
+        "jan":janauary,
+        "apr":April,
+        "mar":march
+    }
+
+
+    return  render(request,'Home/months.html',context)
+
+
+
 
---
+++ readme.md
@@ -0,0 +1,5 @@
+
+# first step 
+- [x] create a new repository on github.
+- [x] create  a virtuel env and install django. ``` virtualenv env ```  ``` source env/bin/activate ``` ``` pip install django```
+
